91fbae58ef1a5e0ab3c2adb68c47939335038222
Vitorgpc <vitorcolazelli@gmail.com>
2020-06-27 01:08:55 -0300

    frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,7 +10,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9.xpczi.mongodb.net
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
+
